7d42d435aa1f3bf25b2d4b4f1462468a132c711c
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-08-12 15:31:27 +0800

    Hello World

---
+++ README.md
@@ -0,0 +1,3 @@
+Reference
+	1.	Greasemonkey in Google Chrome-Tampermonkey
+			~http://www.playpcesor.com/2010/06/google-chromegreasemonkeyblank-canvas.html
---
+++ hw.js
@@ -0,0 +1,33 @@
+(function () {
+
+// ==UserScript==
+// @name          Hello, World!
+// @name          Goodbye, Universe!
+// @namespace     http://localhost.localdomain
+// @icon          http://s3.amazonaws.com/uso_ss/icon/13701/large.jpg
+// @description   JavaScript alert box saying Hello, World!
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @reference	  http://userscripts.org/scripts/review/13701
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @version       0.0.0
+// @version       0.0.1
+// @version       0.0.2
+//
+// @include   http://www.iana.org/*
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+//
+// @exclude   https://www.iana.org/*
+// @exclude   https://example.com/*
+// @exclude   https://example.net/*
+// @exclude   https://example.org/*
+//
+// ==/UserScript==
+
+
+  alert('Hello, World!');
+
+
+})();
